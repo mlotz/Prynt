a479d80fb560e6a5c75376f0eb84b90392f21898
PatrykWasowski <patryk.wasowski10@gmail.com>
2014-12-27 19:01:55 +0100

    Zmiany

--- Program.java
+++ Program.java
@@ -1,9 +1,11 @@
 
-public class Program {
-
-	public static void main(String[] args) {
-		System.out.println("Witaj œwiecie!");
-
+public class Program 
+{
+	public static void main(String[] args) 
+	{
+		System.out.println("Witaj swiecie!");
+		System.out.println("Ciekawe, czy pokazuje zmiany");
 	}
-
 }
+
+
