cdffadd5acac96c96f7d3281a3d134e61dedee04
PatrykWasowski <patryk.wasowski10@gmail.com>
2015-01-05 17:34:34 +0100

    asd

---
+++ GUI.java
@@ -0,0 +1,78 @@
+import java.awt.*;
+import java.awt.geom.*;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+import javax.swing.Timer;
+
+
+public class GUI extends JPanel implements ActionListener
+{
+	private Timer timer;
+	int t;
+	int u;
+	Pret pr;
+	
+	public GUI()
+	{
+		setFocusable(true);
+		setBackground(Color.BLACK);
+		setDoubleBuffered(true);
+		setSize(900, 560);
+		t= 100;
+		u = 0;
+		timer = new Timer(10, this);
+		timer.start();
+		///
+		pr = new Pret(2, 4);
+		Modul_ewolucyjny evo = new Modul_ewolucyjny(2,4);
+		evo.losujWagi();
+		pr.symuluj(evo.wagi, evo.wagiWyjscie);
+	}
+	
+	
+	public void paint(Graphics g)
+	{
+		super.paint(g);
+		Graphics2D g2d = (Graphics2D)g;
+		g.setColor(Color.white);
+		g.setFont(new Font("Arial", Font.BOLD, 40));
+		g.fillRect(t, 1, 10, 10);
+		g2d.drawString("Obiekt 14 - Prent", 15, 40);
+		g.setFont(new Font("Arial", Font.BOLD, 10));
+		g2d.drawString("t:"+ t, t+20, 10);
+		int centerX,centerY,radius ;
+		centerX = centerY = 200;
+		radius = 100;
+		Shape theCircle = new Ellipse2D.Double(centerX - radius, centerY - radius, 2.0 * radius, 2.0 * radius);
+		g2d.draw(theCircle);
+		double pX, pY,pR;
+		double alfa;
+		//alfa = 180;
+		alfa = u;
+		double rads ;
+		rads = Math.toRadians(alfa);
+		//rads = pr.alfy[t];
+		pR = 20;
+		pX = centerX + (int)(radius * Math.sin(rads));
+		pY = centerY - (int)(radius * Math.cos(rads));
+		g.setColor(Color.red);
+		g.fillRect(50+u, 50, 10, 10);
+		g.setFont(new Font("Arial", Font.BOLD, 10));
+		g2d.drawString("angle:"+ u, 70+u, 50);
+		Shape pointer= new Ellipse2D.Double(pX - pR, pY - pR, 2.0 * pR, 2.0 * pR);
+		g2d.draw(pointer);
+		g2d.draw(new Line2D.Double(centerX,centerY,pX,pY));
+		g.dispose();
+	}
+	public void actionPerformed(ActionEvent e)
+	{
+		if (t>=1000) t=0; else t++;
+		if (u>=360) u=0; else u++;
+		//System.out.print(pr.alfy[t] + "\n");
+		repaint();
+	}
+}
--- Program.java
+++ Program.java
@@ -1,26 +1,39 @@
 
-public class Program 
+import javax.swing.JFrame;
+import java.awt.*;
+
+public class Program extends JFrame
 {
-	public static void main(String[] args) 
+	public Program() 
 	{
+		add(new GUI());
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setSize(900, 560);
+		setLocationRelativeTo(null);
+		setTitle("Prent");
+		setResizable(false);
+		setVisible(true);
+	}
+	public static void main(String[] args)
+	{
+		new Program();
 		SiecNeuronowaPretu pret = new SiecNeuronowaPretu(2, 4);
 		double[][] wagi = new double [4][3];
 		double[] wagi_out = new double[5];
 		for (int i = 0; i<4 ; ++i)
-			for (int j = 0; j<3 ; ++j)
-				wagi[i][j]=1.0;
+		for (int j = 0; j<3 ; ++j)
+		wagi[i][j]=1.0;
 		wagi_out[0]= 1.0 ;
 		wagi_out[1]= 1.0 ;
 		wagi_out[2]= 1.0 ;
 		wagi_out[3]= 1.0 ;
 		wagi_out[4]= 1.0 ;
-		
-		Modul_ewolucyjny evo = new Modul_ewolucyjny(2,4);
-		evo.losujWagi();
+		//Modul_ewolucyjny evo = new Modul_ewolucyjny(2,4);
+		//evo.losujWagi();
 		//pret.przypiszWagi(wagi, wagi_out);
-		evo.ewoluuj(0.1);
-		pret.przypiszWagi(evo.podajWagi(), evo.podajWagiWyjscia());
-		pret.pokazWagi();
+		//evo.ewoluuj(0.1);
+		//pret.przypiszWagi(evo.podajWagi(), evo.podajWagiOut());
+		//pret.pokazWagi();
 		double[] input = new double[2];
 		input[0]= 1.0;
 		input[1]= 1.0;
